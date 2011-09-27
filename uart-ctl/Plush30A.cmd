9b44443d93dca8a5f0253ed646dc0ed4d1aee163
joey.moreland@gmail.com <joey.moreland@gmail.com@5c42c5a8-900f-5b5a-2f8c-d720ba63385b>
2011-09-27 00:25:40 +0000

    branch of the main line - will make uart-ctl attempt here before trying to push into main line
    git-svn-id: https://wii-esc.googlecode.com/svn/branches/dnaleromj@6 5c42c5a8-900f-5b5a-2f8c-d720ba63385b

---
+++ uart-ctl/Plush30A.cmd
@@ -0,0 +1,7 @@
+@Echo Off
+call _make.cmd Plush30A pwm_rc_200
+pause
+avrdude.exe -C "H:\Program Files\arduino-0022\hardware\tools\avr\etc\avrdude.conf" -v -p m8 -P com3  -c avrisp -b 19200 -U flash:w:bin\Plush30A.hex 
+
+
+
