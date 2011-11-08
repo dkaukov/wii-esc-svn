17651ed0bfc8b6c6ccbc2f0fd8895940c7636612
d.kayukov@gmail.com <d.kayukov@gmail.com@5c42c5a8-900f-5b5a-2f8c-d720ba63385b>
2011-11-08 02:29:32 +0000

    Experimental: During commutation always switch FETs, but reset commutation cycle.
    git-svn-id: https://wii-esc.googlecode.com/svn/branches/Experemental@121 5c42c5a8-900f-5b5a-2f8c-d720ba63385b

---
+++ ResetPWM/_make.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+SET AS="%ProgramFiles%\AvrAssembler2\avrasm2.exe"
+SET AS_FLAGS= -I "%ProgramFiles%\AvrAssembler2\AppNotes"
+SET OUT=bin
+
+%AS% %AS_FLAGS% -i "m8def.inc" -i "..\input\%2.inc" -i "..\hw\%1.inc"  -S "%OUT%\labels.tmp" -fI -W+ie -o "%OUT%\%1.hex" -d "%OUT%\%1.obj" -m "%OUT%\%1.map" -l "%OUT%\%1.lst" "core\bldc.asm"
