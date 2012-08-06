58c034df0aa134d6adb69238fedf1ae17718dbbc
d.kayukov@gmail.com <d.kayukov@gmail.com@5c42c5a8-900f-5b5a-2f8c-d720ba63385b>
2012-08-06 13:32:36 +0000

    wii-esc-ng:
    As transition start->run is fixed, we do not need so high RPM.
    
    git-svn-id: https://wii-esc.googlecode.com/svn/branches/wii-esc-ng@204 5c42c5a8-900f-5b5a-2f8c-d720ba63385b

--- src/config.h
+++ src/config.h
@@ -17,7 +17,7 @@
 #define    RPM_STEP_MAX        200
 #define    PCT_PWR_STARTUP     7
 #define    PCT_PWR_MAX_STARTUP 30
-#define    RPM_START_MIN_RPM   9000
+#define    RPM_START_MIN_RPM   5000
 #define    ENOUGH_GOODIES      120
 
 //*************************
