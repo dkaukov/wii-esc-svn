f8ad3472d5fcdcc2bbf10515f0a409f30690921c
d.kayukov@gmail.com <d.kayukov@gmail.com@5c42c5a8-900f-5b5a-2f8c-d720ba63385b>
2012-08-06 23:51:39 +0000

    wii-esc-ng:
    ZC filter table in flash (+1 cycle)
    
    git-svn-id: https://wii-esc.googlecode.com/svn/branches/wii-esc-ng@206 5c42c5a8-900f-5b5a-2f8c-d720ba63385b

--- src/zc.h
+++ src/zc.h
@@ -6,10 +6,10 @@
 static uint8_t zc_filter_start;
 static uint8_t zc_filter_run;
 
-const uint8_t zc_filter_table[64]=   {0 ,2 ,4 ,6 ,8 ,10,12,14,16,18,20,22,24,26,28,30,
-                                  	32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,
-                                  	0 ,2 ,4 ,6 ,8 ,10,12,14,16,18,1 ,22,1 ,26,28,30,
-                                  	32,34,36,38,1 ,42,44,46,1 ,1 ,1 ,54,56,58,60,62};
+const uint8_t PROGMEM zc_filter_table[64]=   {0 ,2 ,4 ,6 ,8 ,10,12,14,16,18,20,22,24,26,28,30,
+                                              32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,
+                                              0 ,2 ,4 ,6 ,8 ,10,12,14,16,18,1 ,22,1 ,26,28,30,
+                                              32,34,36,38,1 ,42,44,46,1 ,1 ,1 ,54,56,58,60,62};
 
 
 void update_timing(uint16_t tick) {
@@ -57,7 +57,7 @@ inline void zc_filter_run_reset() {
 inline uint8_t zc_run_detected() {
   uint8_t v = zc_filter_run;
   if (!pwr_stage.aco) v |= 0x01;
-  v = zc_filter_table[v];
+  v = pgm_read_byte(&zc_filter_table[v]);
   zc_filter_run = v;
   return (v & 0x01);
 }
